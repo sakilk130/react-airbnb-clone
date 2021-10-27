83759d4d1db6f3d38daf371ba8eca9af1d05260e
sakilk130 <sakilk130@gmail.com>
2021-10-27 23:51:35 +0600

    create banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,31 @@
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
+
+import '../styles/Banner.css';
+
+function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        <Button
+          className="banner__searchButton"
+          variant="outlined"
+          onClick={() => setShowSearch(!showSearch)}
+        >
+          {showSearch ? 'Hide' : 'Search Dates'}
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get out and stretch your imagination</h1>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you.
+        </h5>
+        <Button>Explore Nearby</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../styles/Home.css';
+import Banner from './Banner';
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
 }
 
 export default Home;
