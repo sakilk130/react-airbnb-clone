4f95376b4ce5253e8d0d5f73f5b07fd6c0f82df7
Sakil Khan <sakilk130@gmail.com>
2021-10-28 10:07:16 +0600

    complete footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
+import Footer from './components/Footer';
 import './styles/App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
