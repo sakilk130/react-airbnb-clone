227480d54823beba1f45099b246775589d86d7bd
Sakil Khan <sakilk130@gmail.com>
2021-10-28 10:24:03 +0600

    complete searchpage and searchresults components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import './styles/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './components/SearchPage';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
